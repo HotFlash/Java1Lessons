201431d64e4ab479bdb9024109d2f6a564118816
Roman Nabokov <roman@newage.kz>
2022-03-07 23:04:45 +0600

    homework lesson3 Reformatted

--- src/ru/geekbrains/lesson3/HomeWorkApp.java
+++ src/ru/geekbrains/lesson3/HomeWorkApp.java
@@ -4,27 +4,28 @@ import java.util.Arrays;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        int[] array = {0,1,1,0,1,1,0,0,0,0,1}; //массив для переназначения
+        int[] array = {0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1}; //массив для переназначения
         arrayRebuild(array);
         System.out.println(Arrays.toString(array)); //проверка переназначения
         int[] array100 = fillArray(100); //заполнение массива
         System.out.println(Arrays.toString(array100)); // проверка заполнения
-        int[] arrayMultiple = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 }; //массив для умножения значений меньше 6
+        int[] arrayMultiple = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}; //массив для умножения значений меньше 6
         arrayMultiple(arrayMultiple);
-        int[][] quarterArray = new int [10][10]; //массив для изменения значений по диагоналям
+        int[][] quarterArray = new int[10][10]; //массив для изменения значений по диагоналям
         quarterArrayFill(quarterArray);
         int[] builderArray = buildArray(10, 200); // заполнение указанной длины массива указанным значением
         System.out.println(Arrays.toString(builderArray)); // проверка заполнения
         minMaxParser(arrayMultiple);
-        int[] isTwoPartSame = {10,5,4,3,3,1,20,4}; //массив для сравнения частей
+        int[] isTwoPartSame = {10, 5, 4, 3, 3, 1, 20, 4}; //массив для сравнения частей
         System.out.println(isTwoPartArraySame(isTwoPartSame, 5)); //проверка частей массива с левой частью в 5 ячеек
     }
+
     public static boolean isTwoPartArraySame(int[] array, int separator) { //проверка двух частей массива на равество
         boolean isSame = false;
         int partLeftSum = 0;
         int partRightSum = 0;
         for (int i = 0; i < array.length; i++) {
-            if ( i < separator ) {
+            if (i < separator) {
                 partLeftSum += array[i];
             } else {
                 partRightSum += array[i];
@@ -35,7 +36,7 @@ public class HomeWorkApp {
         } else {
             isSame = false;
         }
-       return  isSame;
+        return isSame;
     }
 
     public static void minMaxParser(int[] array) { //поиск наибольшего и нгаименьшего числа в массиве
@@ -44,24 +45,23 @@ public class HomeWorkApp {
         for (int i = 0; i < array.length; i++) {
             if (array[i] >= max) {
                 max = array[i];
-            } else if ( min >= array[i]) {
+            } else if (min >= array[i]) {
                 min = array[i];
             }
         }
         System.out.println("наименьшее число: " + min + "\n наибольшее число: " + max);
     }
 
-    public static int[] buildArray(int len, int initialValue){ //заполнение массива по входящей длинне и значению
-        int[] array = new int [len];
+    public static int[] buildArray(int len, int initialValue) { //заполнение массива по входящей длинне и значению
+        int[] array = new int[len];
         for (int i = 0; i < len; i++) {
             array[i] = initialValue;
         }
         return array;
     }
 
-
     public static void quarterArrayFill(int[][] array) { //изменение значений массива по диагоналям
-        int revers = (array.length -1);
+        int revers = (array.length - 1);
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (i == j) {
@@ -85,15 +85,16 @@ public class HomeWorkApp {
             System.out.println();
         }
     }
-    
+
     public static void arrayMultiple(int[] array) { //умножение чисел меньших 6 на 2
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < 6 ) {
+            if (array[i] < 6) {
                 array[i] = array[i] * 2;
             }
         }
         System.out.println(Arrays.toString(array));
     }
+
     public static int[] fillArray(int cellAmount) { //заполнение массива указанной длинны цифрамми по порядку
         int[] array = new int[cellAmount];
         int j = 1;
@@ -103,13 +104,16 @@ public class HomeWorkApp {
         }
         return array;
     }
+
     public static int[] arrayRebuild(int[] array) { //переназначене значиний в массиве
         for (int i = 0; i < array.length; i++) {
             switch (array[i]) {
-                case 0: array[i] = 1;
-                break;
-                case 1: array[i] = 0;
-                break;
+                case 0:
+                    array[i] = 1;
+                    break;
+                case 1:
+                    array[i] = 0;
+                    break;
             }
         }
         return array;
